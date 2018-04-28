64deb550c045a32d4e3ed96e20c0ed6bacd834a0
Paulson  Ps <paulsonps@neoito.com>
2018-04-28 22:35:45 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,348 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      color: Colors.white,
+      theme: defaultTargetPlatform == TargetPlatform.iOS //new
+          ? kIOSTheme //new
+          : kDefaultTheme, //new
+      home: new RandomWords(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _saved = new Set<WordPair>();
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Neoito Flutter Demo'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.thumb_up), onPressed: _pushSaved),
+          new IconButton(icon: new Icon(Icons.chat), onPressed: _chatPlace)
+        ],
+      ),
+      drawer: new Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: new ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new DrawerHeader(
+              child: new Text('Drawer Header'),
+              decoration: new BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            new ListTile(
+              title: new Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            new ListTile(
+              title: new Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.thumb_down : Icons.thumb_up,
+        color: alreadySaved ? Colors.orangeAccent[700] : null,
+      ),
+      onTap: () {
+        setState(
+          () {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          },
+        );
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  void _chatPlace() {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new SecondScreen()),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  @override
+  State createState() => new SecondScreenState();
+}
+
+class SecondScreenState extends State<SecondScreen>
+    with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController =
+      new TextEditingController(); //new
+  bool _isComposing = false;
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text('Friends Chat'),
+          elevation: Theme.of(context).platform == TargetPlatform.iOS
+              ? 0.0
+              : 4.0, //new
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.thumb_up)),
+            new IconButton(icon: new Icon(Icons.chat))
+          ],
+          automaticallyImplyLeading: false),
+      body: new Container(
+          //modified
+          child: new Column(
+            //modified
+            children: <Widget>[
+              new Flexible(
+                child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              new Divider(height: 1.0),
+              new Container(
+                decoration:
+                    new BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS //new
+              ? new BoxDecoration(
+                  //new
+                  border: new Border(
+                    //new
+                    top: new BorderSide(color: Colors.grey[200]), //new
+                  ), //new
+                ) //new
+              : null), //modified
+    );
+  }
+
+  void dispose() {
+    //new
+    for (ChatMessage message in _messages) //new
+      message.animationController.dispose(); //new
+    super.dispose(); //new
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  //new
+                  setState(() {
+                    //new
+                    _isComposing = text.length > 0; //new
+                  }); //new
+                }, //new
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? //modified
+                    new CupertinoButton(
+                        //new
+                        child: new Text("Send"), //new
+                        onPressed: _isComposing //new
+                            ? () => _handleSubmitted(_textController.text) //new
+                            : null,
+                      )
+                    : //new
+                    new IconButton(
+                        //modified
+                        icon: new Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      //new
+      _isComposing = false; //new
+    }); //new
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+}
+
+const String _name = "Paulson Ps";
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+        //new
+        sizeFactor: new CurvedAnimation(
+            //new
+            parent: animationController,
+            curve: Curves.easeOut), //new
+        axisAlignment: 0.0, //new
+        child: new Container(
+          //modified
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Expanded(
+                //new
+                child: new Column(
+                  //modified
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: new TextStyle(color: Colors.red)),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ) //new
+        );
+  }
+}
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
